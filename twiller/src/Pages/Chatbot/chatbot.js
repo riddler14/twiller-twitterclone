5da847042888536b596c4d85f505244c176a2a06
riddler14 <adityaguddad12@gmail.com>
2025-01-06 00:02:46 +0530

    Updating Design of chatbot

--- twiller/src/Pages/Chatbot/chatbot.js
+++ twiller/src/Pages/Chatbot/chatbot.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../pages.css";
 const chatbot = () => {
-  return (<div>
+  return (
     <div className="page">
       <h2 className="pageTitle">Welcome to Chatbot</h2>
-    </div>
+   
   </div>);
 };
 

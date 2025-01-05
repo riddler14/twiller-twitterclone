d69153655a4cd60c97d028f3e8c733316bd6a948
riddler14 <adityaguddad12@gmail.com>
2025-01-05 23:58:40 +0530

    Added Chatbot Page

--- twiller/src/Pages/Chatbot/chatbot.js
+++ twiller/src/Pages/Chatbot/chatbot.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import "../pages.css";
 const chatbot = () => {
   return (<div>
-    
+    <div className="page">
+      <h2 className="pageTitle">Welcome to Chatbot</h2>
+    </div>
   </div>);
 };
 

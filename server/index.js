f518717939a5755e45e652fe89711ea2a58eaf9c
riddler14 <adityaguddad12@gmail.com>
2025-01-10 23:09:29 +0530

    New Tweet embed

--- server/index.js
+++ server/index.js
@@ -5,52 +5,19 @@ const url =
   "mongodb+srv://admin:admin@twitter.3aijc.mongodb.net/?retryWrites=true&w=majority&appName=twitter";
 const port = 5000;
 const axios = require("axios");
-const passport = require("passport");
-const TwitterStrategy = require("passport-twitter").Strategy;
-const session = require("express-session");
-const Bottleneck = require("bottleneck");
 
-const NodeCache = require("node-cache");
-const BEARER_TOKEN =
-  "AAAAAAAAAAAAAAAAAAAAAAbGxwEAAAAAg4LFbxNVXMhHWl5nMREorp%2FU3gw%3Dbs727pAIVl9NbydzOPEQzewVcSCf2OlyruIw4XsSXwP2tf5Ys6";
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(url);
 
-const limiter = new Bottleneck({
-  minTime: 1000,
-  maxConcurrent: 1,
-});
 
-const cache = new NodeCache({ stdTTL: 600, checkperiod: 60 });
 
-app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(
-  new TwitterStrategy(
-    {
-      consumerKey: "pvKwnLQu1WbsXEn3oukKdKgBW",
-      consumerSecret: "FrTp07icR5o3fn15JvFMqUxAO4pzdJLTNqi9DOCswcR5upCNOE",
-      callbackURL:
-        "https://twiller-twitterclone-ewhk.onrender.com/auth/twitter/callback",
-    },
-    function (token, tokenSecret, profile, done) {
-      return done(null, profile);
-    }
-  )
-);
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
 
 async function run() {
   try {
@@ -107,36 +74,19 @@ async function run() {
     // Endpoint to fetch tweets for the chatbot
     app.get("/tweets", async (req, res) => {
       const query = req.query.q;
-      const cachedTweets = cache.get(query);
-      if (cachedTweets) {
-        return res.json(cachedTweets);
-      }
-
-      const url = `https://cdn.syndication.twimg.com/tweet-result?id=${encodeURIComponent(query)}`;
+      const url = `https://cdn.syndication.twimg.com/tweet-result?id=${encodeURIComponent(
+        query
+      )}`;
       try {
-        const response = await limiter.schedule(() =>
-          axios.get(url)
-        );
-        cache.set(query, response.data);
+        const response = await axios.get(url);
         res.json(response.data);
       } catch (error) {
         console.error("Error fetching tweets:", error);
-        res.status(500).send({
-          message: "Error fetching tweets",
-          error: error.response ? error.response.data : error.message,
-        });
+        res.status(500).send("Error fetching tweets");
       }
     });
 
-    app.get("/auth/twitter", passport.authenticate("twitter"));
-
-    app.get(
-      "/auth/twitter/callback",
-      passport.authenticate("twitter", { failureRedirect: "/" }),
-      function (req, res) {
-        res.redirect("/");
-      }
-    );
+    
   } catch (error) {
     console.log(error);
   }

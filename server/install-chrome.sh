8e457a813f8f4f5a9be8f44d6905fbcae69c424d
riddler14 <adityaguddad12@gmail.com>
2025-01-12 23:37:29 +0530

    Removed Script

--- server/install-chrome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Chrome without sudo
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb

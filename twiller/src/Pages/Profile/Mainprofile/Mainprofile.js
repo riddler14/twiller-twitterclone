2ae80fef6c984293303ef6118a4d5f1fb516f53e
riddler14 <adityaguddad12@gmail.com>
2025-02-03 23:03:04 +0530

    Removed Dependency Array

--- twiller/src/Pages/Profile/Mainprofile/Mainprofile.js
+++ twiller/src/Pages/Profile/Mainprofile/Mainprofile.js
@@ -279,7 +279,7 @@ const Mainprofile = ({ user }) => {
     setSkinColorOptions(seedParameters[currentSeed].skinColor);
     setBackgroundOptions(seedParameters[currentSeed].backgroundColor);
       
-  }, [seedParameters]);
+  });
 
   const generateAvatarUrl = () => {
     const params = [];

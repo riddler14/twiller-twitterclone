427edbf3d72f7e66b75ddae313aabfa3ea173a9b
riddler14 <adityaguddad12@gmail.com>
2025-03-16 18:24:43 +0530

    added logic for notifications

--- twiller/src/context/SocketContext.js
+++ twiller/src/context/SocketContext.js
@@ -1,7 +1,7 @@
 // File: src/context/SocketContext.js
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import { useAuth } from "../context/UserAuthContext"; // Import your UserAuthContext
+import { useUserAuth } from "../context/UserAuthContext"; // Import your UserAuthContext
 
 const SocketContext = createContext();
 
@@ -9,7 +9,7 @@ export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
-  const { loggedinuser } = useAuth(); // Retrieve loggedinuser from UserAuthContext
+  const { loggedinuser } = useUserAuth(); // Retrieve loggedinuser from UserAuthContext
   const userEmail = loggedinuser?.email; // Extract the user's email
 
   useEffect(() => {

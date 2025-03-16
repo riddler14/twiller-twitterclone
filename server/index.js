bc24964d8b9bbf835c4e339c99a3a9006e2320de
riddler14 <adityaguddad12@gmail.com>
2025-03-16 23:22:28 +0530

    added logic for notifications

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ io.on("connection", (socket) => {
   const userEmail = socket.handshake.query.email;
 
   if (userEmail) {
-    const decodedEmail = decodeURIComponent(userEmail);
+    const decodedEmail = encodeURIComponent(userEmail);
     console.log("Decoded email:", decodedEmail);
 
     // Join a room named after the user's email
